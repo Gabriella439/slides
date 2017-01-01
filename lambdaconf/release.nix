9b322af677c7ba4bff3fc1234a66b9642617501c
Gabriel Gonzalez <Gabriel439@gmail.com>
2017-01-01 08:46:28 -0800

    Fix figures

--- lambdaconf/release.nix
+++ lambdaconf/release.nix
@@ -4,11 +4,14 @@ let
 in
   { category =
       pkgs.runCommand "category.html" {} ''
-        ${pkgs.pandoc}/bin/pandoc -t slidy -s ${./category.md} -o $out
+        mkdir -p $out
+        ${pkgs.pandoc}/bin/pandoc -t slidy -s ${./category.md} -o $out/category.html
       '';
 
     data =
       pkgs.runCommand "data.html" {} ''
-        ${pkgs.pandoc}/bin/pandoc -t slidy -s ${./data.md} -o $out
+        mkdir -p $out
+        cp ${./chart0.svg} $out/chart0.svg
+        ${pkgs.pandoc}/bin/pandoc -t slidy -s ${./data.md} -o $out/data.html
       '';
   }
