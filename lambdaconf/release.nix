d11e2d7123428e5a0a9dc81647dfccddce149e9b
Gabriel Gonzalez <Gabriel439@gmail.com>
2017-01-01 10:52:46 -0800

    Rename Nix derivations

--- lambdaconf/release.nix
+++ lambdaconf/release.nix
@@ -15,13 +15,13 @@ let
 
 in
   { category =
-      pkgs.runCommand "category.html" {} ''
+      pkgs.runCommand "category-slides" {} ''
         mkdir -p $out
         ${pkgs.pandoc}/bin/pandoc -t slidy -s ${./category.md} -o $out/category.html
       '';
 
     data =
-      pkgs.runCommand "data.html" {} ''
+      pkgs.runCommand "data-slides" {} ''
         mkdir -p $out
         ln -s ${./cucumber0.jpeg} $out/cucumber0.jpeg
         ln -s ${./chart0.svg}     $out/chart0.svg
